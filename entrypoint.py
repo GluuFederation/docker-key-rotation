e03c299667e24d280e9b163564ce2f883bee74d1
iromli <isman.firmansyah@gmail.com>
2019-04-11 22:37:17 +0700

    conform to internal oxAuth keyregeneration feature

--- entrypoint.py
+++ entrypoint.py
@@ -2,7 +2,9 @@ import base64
 import json
 import logging
 import os
+import random
 import shlex
+import string
 import subprocess
 import time
 
@@ -22,6 +24,11 @@ GLUU_KEY_ROTATION_INTERVAL = os.environ.get("GLUU_KEY_ROTATION_INTERVAL", 48)
 
 # check interval (by default per 1 hour)
 GLUU_KEY_ROTATION_CHECK = os.environ.get("GLUU_KEY_ROTATION_CHECK", 60 * 60)
+
+# Default charset
+_DEFAULT_CHARS = "".join([string.ascii_uppercase,
+                          string.digits,
+                          string.lowercase])
 
 
 logger = logging.getLogger("key_rotation")
@@ -75,6 +82,13 @@ def should_rotate_keys():
     except ValueError:
         rotation_interval = 48
 
+    # due to the limitation on KeyGenerator CLI where expiration
+    # is set to daily, ensure interval always a multiplication of 24 (hours)
+    if rotation_interval % 24 != 0:
+        logger.warn("GLUU_KEY_ROTATION_INTERVAL value only support "
+                    "a value of multiplication of 24")
+        return False
+
     # use default rotation interval if the number is less than equal 0
     if rotation_interval <= 0:
         rotation_interval = 48
@@ -89,14 +103,6 @@ def should_rotate_keys():
     return now > next_rotation
 
 
-def get_ldap_servers():
-    servers = []
-    for server in GLUU_LDAP_URL.split(","):
-        host, port = server.split(":", 1)
-        servers.append({"host": host, "port": port})
-    return servers
-
-
 def decrypt_text(encrypted_text, key):
     cipher = pyDes.triple_des(b"{}".format(key), pyDes.ECB,
                               padmode=pyDes.PAD_PKCS5)
@@ -104,75 +110,21 @@ def decrypt_text(encrypted_text, key):
     return cipher.decrypt(encrypted_text)
 
 
-def modify_oxauth_config(pub_keys):
-    # user = "cn=directory manager,o=gluu"
-    user = config_manager.get("ldap_binddn")
-    passwd = decrypt_text(config_manager.get("encoded_ox_ldap_pw"),
-                          config_manager.get("encoded_salt"))
+def modify_oxauth_config(ldap_conn, entry_dn, ox_rev, conf_dynamic, conf_webkeys):
+    logger.info("modifying oxAuth configuration")
 
-    # base DN for oxAuth config
-    oxauth_base = ",".join([
-        "ou=oxauth",
-        "ou=configuration",
-        "inum={}".format(config_manager.get("inumAppliance")),
-        "ou=appliances",
-        "o=gluu",
-    ])
+    serialized_keys_conf = json.dumps(conf_webkeys)
+    serialized_dyn_conf = json.dumps(conf_dynamic)
+    ox_rev = str(ox_rev + 1)
 
-    for server in get_ldap_servers():
-        try:
-            ldap_server = Server(server["host"], port=int(server["port"]), use_ssl=True)
-            logger.info("connecting to server {}:{}".format(server["host"], server["port"]))
+    ldap_conn.modify(entry_dn, {
+        'oxRevision': [(MODIFY_REPLACE, [ox_rev])],
+        'oxAuthConfWebKeys': [(MODIFY_REPLACE, [serialized_keys_conf])],
+        'oxAuthConfDynamic': [(MODIFY_REPLACE, [serialized_dyn_conf])],
+    })
 
-            with Connection(ldap_server, user, passwd) as conn:
-                conn.search(search_base=oxauth_base, search_filter="(objectClass=*)",
-                            search_scope=BASE, attributes=['*'])
-
-                if not conn.entries:
-                    # search failed due to missing entry
-                    logger.warn("unable to find oxAuth config")
-                    continue
-
-                entry = conn.entries[0]
-
-                # oxRevision is increased to mark update
-                ox_rev = str(int(entry["oxRevision"][0]) + 1)
-
-                # update public keys if necessary
-                keys_conf = json.loads(entry["oxAuthConfWebKeys"][0])
-                keys_conf["keys"] = pub_keys
-                serialized_keys_conf = json.dumps(keys_conf)
-
-                dyn_conf = json.loads(entry["oxAuthConfDynamic"][0])
-                dyn_conf.update({
-                    "keyRegenerationEnabled": False,  # always set to False
-                    "keyRegenerationInterval": int(GLUU_KEY_ROTATION_INTERVAL),
-                    "defaultSignatureAlgorithm": "RS512",
-                })
-                dyn_conf.update({
-                    "webKeysStorage": "keystore",
-                    "keyStoreSecret": config_manager.get("oxauth_openid_jks_pass"),
-                })
-                serialized_dyn_conf = json.dumps(dyn_conf)
-
-                # update the attributes
-                logger.info("modifying oxAuth configuration")
-                conn.modify(entry.entry_dn, {
-                    'oxRevision': [(MODIFY_REPLACE, [ox_rev])],
-                    'oxAuthConfWebKeys': [(MODIFY_REPLACE, [serialized_keys_conf])],
-                    'oxAuthConfDynamic': [(MODIFY_REPLACE, [serialized_dyn_conf])],
-                })
-
-                result = conn.result["description"]
-                return result == "success"
-        except LDAPSocketOpenError as exc:
-            logger.warn("Unable to connect to LDAP at {}:{}; reason={}".format(
-                server["host"], server["port"], exc))
-            logger.info("Trying other server (if possible).")
-            continue
-
-    # mark update as failed
-    return False
+    result = ldap_conn.result["description"]
+    return result == "success"
 
 
 def encode_jks(jks="/etc/certs/oxauth-keys.jks"):
@@ -182,34 +134,87 @@ def encode_jks(jks="/etc/certs/oxauth-keys.jks"):
     return encoded_jks
 
 
-def rotate_keys():
-    out, err, retcode = generate_openid_keys(
-        config_manager.get("oxauth_openid_jks_pass"),
-        config_manager.get("oxauth_openid_jks_fn"),
-        r"{}".format(config_manager.get("default_openid_jks_dn_name")),
-    )
-
-    if retcode != 0:
-        logger.error("unable to generate keys; reason={}".format(err))
-        return False
-
+def rotate_keys(user, passwd, inum, jks_pass, jks_fn, jks_dn):
     try:
-        pub_keys = json.loads(out).get("keys")
-    except (TypeError, ValueError) as exc:
-        logger.warn("unable to get public keys; reason={}".format(exc))
-        return False
+        logger.info("connecting to server {}".format(GLUU_LDAP_URL))
 
-    if all([modify_oxauth_config(pub_keys),
-            config_manager.set("oxauth_jks_base64", encode_jks())]):
-        config_manager.set("oxauth_key_rotated_at", int(time.time()))
-        logger.info("keys have been rotated")
-        return True
+        ldap_server = Server(GLUU_LDAP_URL, port=1636, use_ssl=True)
+
+        with Connection(ldap_server, user, passwd) as conn:
+            # get oxAuth config from LDAP
+            ox_config = get_oxauth_config(conn, inum)
+
+            if not ox_config:
+                # search failed due to missing entry
+                logger.warn("unable to find oxAuth config")
+                return
+
+            ox_rev = int(ox_config["oxRevision"][0])
+
+            conf_dynamic = json.loads(ox_config["oxAuthConfDynamic"][0])
+
+            if conf_dynamic["keyRegenerationEnabled"]:
+                logger.warn("keyRegenerationEnabled config was set to true; "
+                            "skipping proccess to avoid conflict with "
+                            "builtin key rotation feature in oxAuth")
+                return
+
+            conf_dynamic.update({
+                "keyRegenerationEnabled": False,  # always set to False
+                "keyRegenerationInterval": int(GLUU_KEY_ROTATION_INTERVAL),
+                "webKeysStorage": "keystore",
+                "keyStoreSecret": jks_pass,
+                # "defaultSignatureAlgorithm": "RS512",
+            })
+
+            conf_webkeys = json.loads(ox_config["oxAuthConfWebKeys"][0])
+
+            exp_in_days = get_exp_in_days(
+                int(GLUU_KEY_ROTATION_INTERVAL),
+                conf_dynamic["idTokenLifetime"],
+            )
+
+            out, err, retcode = generate_openid_keys(
+                jks_pass, jks_fn, jks_dn, exp=exp_in_days)
+
+            if retcode != 0:
+                logger.error("unable to generate keys; reason={}".format(err))
+                return False
+
+            try:
+                new_keys = json.loads(out)
+                conf_webkeys = merge_keys(new_keys, conf_webkeys)
+                # conf_webkeys = new_keys
+                ox_modified = modify_oxauth_config(
+                    conn, ox_config.entry_dn, ox_rev, conf_dynamic, conf_webkeys)
+
+                if all([ox_modified,
+                        config_manager.set("oxauth_jks_base64", encode_jks())]):
+                    config_manager.set("oxauth_key_rotated_at", int(time.time()))
+                    logger.info("keys have been rotated")
+                    return True
+            except (TypeError, ValueError) as exc:
+                logger.warn("unable to get public keys; reason={}".format(exc))
+                return False
+    # cant connect to LDAP
+    except LDAPSocketOpenError as exc:
+        logger.warn("Unable to connect to LDAP at {}; reason={}".format(
+            GLUU_LDAP_URL, exc))
 
     # mark rotation as failed
     return False
 
 
 def main():
+    inum = config_manager.get("inumAppliance")
+    user = config_manager.get("ldap_binddn")
+    passwd = decrypt_text(config_manager.get("encoded_ox_ldap_pw"),
+                          config_manager.get("encoded_salt"))
+    # jks_pass = config_manager.get("oxauth_openid_jks_pass")
+    jks_pass = get_random_chars()
+    jks_fn = config_manager.get("oxauth_openid_jks_fn")
+    jks_dn = r"{}".format(config_manager.get("default_openid_jks_dn_name"))
+
     try:
         check_interval = int(GLUU_KEY_ROTATION_CHECK)
     except ValueError:
@@ -221,7 +226,7 @@ def main():
 
             try:
                 if should_rotate_keys():
-                    rotate_keys()
+                    rotate_keys(user, passwd, inum, jks_pass, jks_fn, jks_dn)
                 else:
                     logger.info("no need to rotate keys at the moment")
             except Exception as exc:
@@ -236,6 +241,55 @@ def encrypt_text(text, key):
                               padmode=pyDes.PAD_PKCS5)
     encrypted_text = cipher.encrypt(b"{}".format(text))
     return base64.b64encode(encrypted_text)
+
+
+def get_exp_in_days(rotation_interval, token_lifetime):
+    # current version of KeyGenerator CLI only accept `--expiration=<days>`
+    days = (rotation_interval + (token_lifetime / 3600)) / 24
+    # TODO: `days` value might be 0, do we need to set fallback or leave it as is?
+    return days
+
+
+def get_random_chars(size=12, chars=_DEFAULT_CHARS):
+    """Generates random characters.
+    """
+    return ''.join(random.choice(chars) for _ in range(size))
+
+
+def get_oxauth_config(conn, inum):
+    # base DN for oxAuth config
+    oxauth_base = ",".join([
+        "ou=oxauth",
+        "ou=configuration",
+        "inum={}".format(inum),
+        "ou=appliances",
+        "o=gluu",
+    ])
+
+    conn.search(
+        search_base=oxauth_base,
+        search_filter="(objectClass=*)",
+        search_scope=BASE,
+        attributes=[
+            "oxRevision",
+            "oxAuthConfWebKeys",
+            "oxAuthConfDynamic",
+        ]
+    )
+
+    if not conn.entries:
+        return
+    return conn.entries[0]
+
+
+def merge_keys(new_keys, old_keys):
+    """Merges new and old keys while omitting expired key.
+    """
+    now = int(time.time() * 1000)
+    for key in old_keys["keys"]:
+        if key.get("exp") > now:
+            new_keys["keys"].append(key)
+    return new_keys
 
 
 if __name__ == "__main__":
